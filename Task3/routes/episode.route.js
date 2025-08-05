9c0e987a1da915c86bcfc4f33ae65f9b037056ce
Bigfish06 <keenanmenezes2006@gmail.com>
2025-08-06 01:31:50 +0530

    Update episode.route.js

--- Task3/routes/episode.route.js
+++ Task3/routes/episode.route.js
@@ -5,9 +5,15 @@ const controllerFunctions=require('../controllers/episode.controller')
 const createEpisode=controllerFunctions.createEpisode
 const getEpisode=controllerFunctions.getEpisode
 const getEpisodeById=controllerFunctions.getEpisodeById
+const updateEpisodeEntirely=controllerFunctions.updateEpisodeEntirely
+const updateEpisodePartially=controllerFunctions.updateEpisodePartially
+const deleteEpisode=controllerFunctions.deleteEpisode
 
 router.post('/',createEpisode)
 router.get('/',getEpisode)
 router.get('/:id',getEpisodeById)
+router.put('/:id',updateEpisodeEntirely)
+router.patch('/:id',updateEpisodePartially)
+router.delete('/:id',deleteEpisode)
 
 module.exports=router
