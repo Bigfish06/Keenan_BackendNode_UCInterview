4e7d615cda9927c41842753b7f1cf5a81e3de0cd
Bigfish06 <keenanmenezes2006@gmail.com>
2025-08-06 01:30:19 +0530

    Update episode.controller.js

--- Task3/controllers/episode.controller.js
+++ Task3/controllers/episode.controller.js
@@ -25,7 +25,7 @@ const getEpisodeById=async(req,res)=>{
         const episode=await Episode.findOne({id: id})
         if(!episode)
         {
-            res.status(404).json('No such episode found')
+            return res.status(404).json('No such episode found')
         }
         res.status(200).json(episode)
     } catch (error) {
@@ -33,4 +33,49 @@ const getEpisodeById=async(req,res)=>{
     }
 }
 
-module.exports={createEpisode,getEpisode,getEpisodeById}
+const updateEpisodeEntirely=async(req,res)=>{
+    try {
+        const {id}=req.params
+        let updatedEpisode=req.body
+        updatedEpisode.id=id
+        const episode=await Episode.findOneAndReplace({id:id},updatedEpisode,{new:true})
+        if(!episode)
+        {
+            return res.status(404).json('No such episode found')
+        }        
+        res.status(200).json(episode)
+    } catch (error) {
+        res.status(500).json({data:error.message, message:'Failed to update this episode'})
+    }
+}
+
+const updateEpisodePartially=async(req,res)=>{
+    try {
+        const {id}=req.params
+        const episode=await Episode.findOneAndUpdate({id:id},req.body,{new:true})
+        if(!episode)
+        {
+            return res.status(404).json('No such episode found')
+        }
+        res.status(200).json(episode)
+    } catch (error) {
+        res.status(500).json({data:error.message, message:'Failed to update this episode'})
+    }
+}
+
+const deleteEpisode=async(req,res)=>{
+    try {
+        const {id}=req.params
+        const deletionObj=await Episode.deleteMany({id:id})
+        //self-explanatory
+        if(deletionObj.deletedCount==0)
+        {
+            return res.status(404).json('No such episode found')
+        }
+        res.status(200).json('Episode was deleted successfully')
+    } catch (error) {
+        res.status(500).json({data:error.message, message:'Failed to delete this episode'})
+    }
+}
+
+module.exports={createEpisode,getEpisode,getEpisodeById,updateEpisodeEntirely,updateEpisodePartially,deleteEpisode}
